7b264f850bccdd32ec8e46d963cf31c7f888ab7c
songhee24 <sadchill4@gmail.com>
2024-11-07 20:49:21 +0600

    bottom_navigation.dart

--- lib/app/navigation/bottom_navigation.dart
+++ lib/app/navigation/bottom_navigation.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:wb_supplieses/shared/lib/router/config.dart';
 
 class BottomNavigation extends StatelessWidget {
-  const BottomNavigation({super.key});
+  final int currentIndex;
+
+  const BottomNavigation({super.key, required this.currentIndex});
+
+  void _onItemTapped(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        context.go(PathKeys.supplieses());
+        break;
+      case 1:
+        context.go(PathKeys.uploadExelFile());
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BottomNavigationBar(
+        onTap: (index) => _onItemTapped(context, index),
+        currentIndex: currentIndex,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.inventory), label: 'Коробки'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.insert_chart), label: 'Обновить базу')
+        ]);
   }
 }
