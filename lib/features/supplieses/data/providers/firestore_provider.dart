f9cdeac27555793ec02315e24e4f176da34208db
songhee24 <sadchill4@gmail.com>
2024-11-08 22:59:24 +0600

    setup firebase core and firestore

---
+++ lib/features/supplieses/data/providers/firestore_provider.dart
@@ -0,0 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreProvider {
+  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'package:wb_supplieses/app/app.dart';
 import 'package:wb_supplieses/features/supplieses/supplieses.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MultiBlocProvider(providers: [
     BlocProvider(
       create: (_) => SuppliesBloc(),
