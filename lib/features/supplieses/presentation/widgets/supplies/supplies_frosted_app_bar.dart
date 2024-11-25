0622900b74f1d37ddac4f0881ecec3de49f1f3b7
songhee24 <sadchill4@gmail.com>
2024-11-26 04:43:15 +0600

    upd style of appbar

--- lib/features/supplieses/presentation/widgets/supplies/supplies_frosted_app_bar.dart
+++ lib/features/supplieses/presentation/widgets/supplies/supplies_frosted_app_bar.dart
@@ -10,39 +10,42 @@ class FrostedAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const kToolbarHeightCustom = kToolbarHeight * 2;
+    const kToolbarHeightCustom = kToolbarHeight * 2.3;
 
     return DefaultTabController(
       length: 2,
       child: SliverAppBar(
-        toolbarHeight: kToolbarHeightCustom - 40,
+        toolbarHeight: kToolbarHeightCustom - 30,
         automaticallyImplyLeading: showLeading!,
         backgroundColor: Colors.transparent,
         pinned: true,
         floating: true,
         snap: false,
         actions: actions ?? [],
-        flexibleSpace: ClipRect(
-          child: BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-            child: SizedBox(
-              height: kToolbarHeightCustom,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: TabBar(
-                  controller: tabController,
-                  indicatorSize: TabBarIndicatorSize.tab,
-                  padding: const EdgeInsets.only(top: kToolbarHeight + 15),
-                  dividerColor: Colors.transparent,
-                  tabs: const [
-                    Tab(text: 'Созданы'),
-                    Tab(text: 'Загружены'),
-                  ],
-                  indicator: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50), // Creates border
-                      color: Colors.purpleAccent.withOpacity(0.5)),
-                  labelColor: Colors.white,
-                  unselectedLabelColor: Colors.grey,
+        flexibleSpace: Container(
+          color: Colors.grey.withOpacity(0.11),
+          child: ClipRect(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+              child: SizedBox(
+                height: kToolbarHeightCustom + 10,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 16, right: 16, bottom: 12),
+                  child: TabBar(
+                    controller: tabController,
+                    indicatorSize: TabBarIndicatorSize.tab,
+                    padding: const EdgeInsets.only(top: kToolbarHeight + 30),
+                    dividerColor: Colors.transparent,
+                    tabs: const [
+                      Tab(text: 'Созданы'),
+                      Tab(text: 'Загружены'),
+                    ],
+                    indicator: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50), // Creates border
+                        color: Colors.purpleAccent.withOpacity(0.5)),
+                    labelColor: Colors.white,
+                    unselectedLabelColor: Colors.grey,
+                  ),
                 ),
               ),
             ),
